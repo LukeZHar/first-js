2d7a01c5b9ad1a5b4dd5359c3e7af12e4fdc14e9
LukeZHar <15649@coderacademy.edu.au>
2024-10-27 08:52:34 +1000

    Included if statements and different symbol types

--- script.js
+++ script.js
@@ -50,3 +50,57 @@ console.log(person);
 // arr.unshift() will add an element to the start of the array
 // arr.splice() will remove an element from the array
 
+
+// if conditions:
+// if (condition) {
+//     code
+// } else if (condition) {
+//     code
+// } else {
+//     code
+// }
+
+// while (condition) {
+//     code
+// }
+
+// let (coinflip == "heads")
+
+// if (coinflip == "heads") {
+//     console.log("Heads")
+// } else {
+//     console.log("Tails")
+// }
+
+// if (true) {
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+
+// for (let i = 0; i < 10; i++) { this will run the code 10 times
+//     console.log(i) this will print 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+// }
+
+// for (let i = 0; i < 10; i++) { this will run the code 10 times
+//     if (i == 5) { 
+//         break - this will stop the loop
+//     }
+//     console.log(i) this will print 0, 1, 2, 3, 4
+// }
+
+// for (let i = 0; i < 10; i++) { this will run the code 10 times
+//     if (i == 5) { this will stop the loop
+//         continue  - this will skip the loop
+//     }
+//     console.log(i) this will print 0, 1, 2, 3, 4
+// }
+
+// == are values on both side the same even if different data types
+// === are values on both side the same & the same data type
+// < is less than
+// > is greater than
+// <= is less than or equal to
+// >= is greater than or equal to
+// != is not equal to
+// !== is not equal to
