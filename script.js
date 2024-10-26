9f5c290922795c0fd1a31c58274434728609de8b
LukeZHar <15649@coderacademy.edu.au>
2024-10-27 08:34:51 +1000

    Made a index.html and scipt.js gave definitions of different feats

---
+++ script.js
@@ -0,0 +1,52 @@
+console.log ("hello world");
+
+// name.length = shows lenght of the string
+// name.toUpperCase = shows the string in upperCase
+// name.toLowerCase = shows the string in lowerCase
+// name.charAt(0) = shows the first character of the string
+// name.charAt(name.length-1) = shows the last character of the string
+// "" + "" = shows the string together
+// 1 + 2 = shows the sum of the numbers
+// 1 - 2 = shows the difference of the numbers
+// 1 * 2 = shows the product of the numbers
+// 1 / 2 = shows the quotient of the numbers
+// 1 % 2 = shows the remainder of the numbers
+// !true = shows the boolean value - false
+// !false = shows the boolean value - true
+// true && true = shows the boolean value - true
+// true && false = shows the boolean value - false
+// true || true = shows the boolean value - true
+// true || false = shows the boolean value - true
+// true == true = shows the boolean value - true
+// true == false = shows the boolean value - false
+// true != true = shows the boolean value - false
+// true != false = shows the boolean value - true
+// 123 === "123" = shows the boolean value - false
+// 123 == "123" = shows the boolean value - true
+
+const person = {
+        name: "John",
+        age: 30,
+        address: {
+            city: "Brisbane",
+            state: "QLD"
+            },
+        }
+    
+console.log(person);
+
+// person shows the object
+// person.age shows the age of the object
+// person.address shows the address of the object
+// person.address.city shows the city of the address of the object
+// person.address.state shows the state of the address of the object
+// arr arr (3) ['Luke', 30, 4000]
+// arr[1] will show the second element of the array - 30
+// arr.length will show the length of the array - 3
+// arr.include() will show the boolean value - true or false
+// arr.push() will add an element to the end of the array
+// arr.pop() will remove an element from the end of the array
+// arr.shift() will remove an element from the start of the array
+// arr.unshift() will add an element to the start of the array
+// arr.splice() will remove an element from the array
+
